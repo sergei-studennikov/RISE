7d07f191eb06b8f12bf93695998b8e48f06cf1c2
studennikov-serg <48173157+studennikov-serg@users.noreply.github.com>
2026-04-21 09:15:05 +0400

    Clear artefacts script small fix

--- clear-art.sh
+++ clear-art.sh
@@ -2,5 +2,5 @@
 img_name="wa-image"
 cont_name="wa-container"
 docker stop $cont_name>/dev/null 2>&1
-docker remove $cont_name>/dev/null 2>&1
+docker rm $cont_name>/dev/null 2>&1
 docker rmi $img_name
